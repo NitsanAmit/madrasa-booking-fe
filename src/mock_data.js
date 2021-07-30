49c34588a7866b980380108221de97dca21d7098
davidbo <davidbo@il.ibm.com>
2021-07-30 10:31:21 +0300

    data types

---
+++ src/mock_data.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        id: 5,
+        year: "2021",
+        month: "7",
+        day: "20",
+        time: "13:00",
+        students: [
+            10, 20, 6332, 2431
+        ],
+        teachers: [
+            4
+        ],
+        status: "available",
+        info: "Info about event"
+    },
+    {
+        id: 5,
+        year: "2021",
+        month: "7",
+        day: "20",
+        time: "13:00",
+        students: [
+            10, 20, 6332, 2431
+        ],
+        teachers: [
+            4
+        ],
+        status: "available",
+        info: "Info about event"
+    },
+    {
+        id: 5,
+        year: "2021",
+        month: "7",
+        day: "20",
+        time: "13:00",
+        students: [
+            10, 20, 6332, 2431
+        ],
+        teachers: [
+            4
+        ],
+        status: "available",
+        info: "Info about event"
+    }
+]
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface AvailableDate{
+    id: number;
+    year: string;
+    month: string;
+    day: string;
+    time: string;
+    students: number[];
+    teachers: number[];
+    status: string;
+    info: string;
+}
